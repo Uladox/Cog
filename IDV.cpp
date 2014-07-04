0d8bfa8f9c4026bfd3b0f960937814b8bd4d15b0
Uladox <Uladox@github.com>
2014-07-03 20:50:37 -1000

    Terrible hack! Need to fix!!!

--- IDV.cpp
+++ IDV.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
 #include "Cog.h"
 #include <thread>
+#include <list>
+#include <stdlib.h>
+
 using namespace std;
 
 struct Sobject{
   slot inslot;
   bool die;
   thread sthread;
+  void run();
   void end(){
     sthread.join();
-  }
-  void run(){
-    while(die != true){
-      inslot.eval();
-      if(inslot.macromap["die"] == "1")
-	die = true;
-    }
+    cout << "ended";
   }
   void start(){
     sthread = thread (&Sobject::run, this);
   }
 };
+
+list<Sobject*> killist;
+void killem (){
+  while (true) {
+    if(killist.size() != 0){
+      killist.front()->end();
+      killist.pop_front();
+     }
+  }
+}
+thread killthread (killem);
+
+void Sobject::run(){
+    while(die != true){
+      inslot.eval();
+      if(inslot.macromap["die"] == "1")
+	die = true;
+    }
+    killist.push_back(this);
+  }
 
 int main()
 {
@@ -28,7 +46,10 @@ int main()
     a.inslot.set_code("a print  b print \"cat\" stradd print \"1 die\" defmacro");
     a.inslot.slotset();
     a.start();
-    string i;
-    cin >> i;
-    a.end();
+    //hack, DO NOT USE IN REAL PROJECTS!!!!!!
+    string n;
+    cin >> n;
+    exit(EXIT_SUCCESS);
+    cerr << "\nYou have choosen to exit";
+   
 }
