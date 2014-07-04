a2f88d8f5dacaa47de57aeeb4c412e8879049c0c
Uladox <uladoxiental@gmail.com>
2014-07-04 13:18:36 -1000

    I got some mutexs

--- IDV.cpp
+++ IDV.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <list>
 #include <stdlib.h>
+#include <mutex>
 
 using namespace std;
 
@@ -10,10 +11,15 @@ struct Sobject{
   slot inslot;
   bool die;
   thread sthread;
-  void run();
+  void run(){
+    while(die != true){
+      inslot.eval();
+      if(inslot.macromap["die"] == "1")
+	die = true;
+    }
+  }
   void end(){
     sthread.join();
-    cout << "ended";
   }
   void start(){
     sthread = thread (&Sobject::run, this);
@@ -21,23 +27,22 @@ struct Sobject{
 };
 
 
-void Sobject::run(){
-    while(die != true){
-      inslot.eval();
-      if(inslot.macromap["die"] == "1")
-	die = true;
-    }
-  }
+list<Sobject> Sglobal;
+std::mutex Sglobal_mutex;
+void add_sobj(string content){
+  Sglobal_mutex.lock();
+  Sglobal.emplace_back();
+  Sglobal.back().inslot.set_code(content);
+  Sglobal.back().inslot.slotset();
+  Sglobal_mutex.unlock();
+}
 
 int main()
 {
-    Sobject a;
-    a.inslot.set_code("a print  b print \"cat\" stradd print \"1 die\" defmacro");
-    a.inslot.slotset();
-    a.start();
-    //hack, DO NOT USE IN REAL PROJECTS!!!!!!
-    //a.sthread.join();
+  add_sobj("a print \"1 die\" defmacro");
+  Sglobal.back().start();
     while (true) {
       
     }
 }
+
